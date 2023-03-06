1f1667a7482c0bd53ee79345dd425ac63c830e71
va2dim <va2dim@gmail.com>
2023-03-06 15:31:12 +0200

    Add possibility to process YAML multiple docs structure

--- src/Yaml.php
+++ src/Yaml.php
@@ -7,19 +7,41 @@ use Memcrab\File\FileException;
 
 class Yaml extends File
 {
+    const PARSE_MULTIPLE_DOCS = [
+        'ONE' => 0,
+        'ALL' => -1 
+    ];
+
     public function load(string $filePath): File
     {
         $this->parseYamlFile($filePath);
         return $this;
     }
 
-    private function parseYamlFile(string $filePath)
+    public function loadMultipleDocs(string $filePath, bool $flattenParcingResult = true, bool $flattenRecursevly = true): File
+    {
+        $this->parseYamlFile($filePath, true, $flattenParcingResult, $flattenRecursevly);
+        return $this;
+    }
+
+    private function parseYamlFile(string $filePath, bool $parseMultipleDocs = false, bool $flattenMultipleDocsParsingResult = false, bool $flattenRecursevly = false)
     {
         $this->checkFilePath($filePath);
 
-        $this->content = \yaml_parse_file($filePath);
+        $this->content = \yaml_parse_file($filePath, ($parseMultipleDocs === true) ? self::PARSE_MULTIPLE_DOCS['ALL'] : self::PARSE_MULTIPLE_DOCS['ONE']);        
+
         if ($this->content === false) {
             throw new FileException(_("Can't parse yaml content from file:") . " " . $filePath, 501);
+        }
+        
+        if ($flattenMultipleDocsParsingResult === true && is_array($this->content)) {
+            $nonEmptyArrays = array_filter($this->content, function($element) {
+                return is_array($element) && !empty($element);
+            });
+
+            $this->content = ($flattenRecursevly === true) 
+                                ? array_merge_recursive([], ...$nonEmptyArrays)
+                                : array_merge([], ...$nonEmptyArrays);
         }
     }
 }
